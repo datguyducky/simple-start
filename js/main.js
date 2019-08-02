579c3e4af5d3c76abc82293e84e5c15f0dd4ed93
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2019-08-02 06:55:16 +0200

    Beautify

--- js/main.js
+++ js/main.js
@@ -7,36 +7,37 @@ function idNewTagSearch(bookmarkItems) {
     }
 }
 
-var gettingID = browser.bookmarks.search({title : "speeddial"}); //searching for default speeddial bookmarks folder
+//searching for default speeddial bookmarks folder
+var gettingID = browser.bookmarks.search({
+    title: "speeddial"
+}); 
 gettingID.then(idNewTagSearch);
 
 function tagMaker(children) {
     for (child of children) {
-      if(child.type == "folder"){
-        //console.log(child.title);
+        if (child.type == "folder") {
+            //console.log(child.title);
 
-        var tagsLeft = document.getElementById("nav-tags-list-left");
-        var tagsRight = document.getElementById("nav-tags-list-right");
-        
-        leftCount = tagsLeft.childElementCount; //getting number of elements for left tag list
-        rightCount = tagsRight.childElementCount; //getting number of elements for right tag list
+            var tagsLeft = document.getElementById("nav-tags-list-left");
+            var tagsRight = document.getElementById("nav-tags-list-right");
 
-        if(leftCount == 0){
-            new_tag = document.createElement('li');
-            tagsLeft.appendChild(new_tag);
+            leftCount = tagsLeft.childElementCount; //getting number of elements for left tag list
+            rightCount = tagsRight.childElementCount; //getting number of elements for right tag list
+
+            if (leftCount == 0) {
+                new_tag = document.createElement('li');
+                tagsLeft.appendChild(new_tag);
+            } else if (leftCount > rightCount) {
+                new_tag = document.createElement('li');
+                tagsRight.appendChild(new_tag);
+            } else {
+                new_tag = document.createElement('li');
+                tagsLeft.appendChild(new_tag);
+            }
+            new_tag.setAttribute("class", "nav-tag-li");
+            new_tag.setAttribute("id", child.title);
+            new_tag.innerHTML = child.title;
         }
-        else if(leftCount>rightCount){
-            new_tag = document.createElement('li');
-            tagsRight.appendChild(new_tag);
-        }
-        else{
-            new_tag = document.createElement('li');
-            tagsLeft.appendChild(new_tag);
-        }
-        new_tag.setAttribute("class", "nav-tag-li");
-        new_tag.setAttribute("id", child.title);
-        new_tag.innerHTML = child.title;
-      }
     }
     //listener for 'click' on nav element
     var navList = document.getElementsByClassName("nav-tag-li");
@@ -50,7 +51,9 @@ function tagMaker(children) {
             navList[i].classList.remove('active');
         }
         document.getElementById(this.id).classList.add('active');
-        var tagIDSearch = browser.bookmarks.search({title : this.id});
+        var tagIDSearch = browser.bookmarks.search({
+            title: this.id
+        });
         tagIDSearch.then(tagFolderID);
     }
 }
@@ -100,7 +103,7 @@ function cardMaker(url, title) {
         document.getElementById("bookmarks").appendChild(clickable);
     }*/
 
-    /*var card_title = document.createElement('div');
+/*var card_title = document.createElement('div');
     var card_image = document.createElement('div');
     var clickable = document.createElement('a');
 
@@ -128,14 +131,15 @@ document.getElementById('user-button-settings').onclick = function changeContent
 
     var box = document.getElementById("user-settings");
 
-    if(box.style.display == "flex") {
+    if (box.style.display == "flex") {
         box.style.display = "none";
+    } else {
+        box.style.display = "flex";
     }
-    else{box.style.display = "flex";}
 }
 
-  
-function restoreOptions() { 
+
+function restoreOptions() {
     function setCurrentChoice(result) {
         document.documentElement.style.setProperty('--darkColor', result.navColor);
         document.documentElement.style.setProperty('--headerColor', result.textColor);
@@ -147,7 +151,7 @@ function restoreOptions() {
 
     var getting = browser.storage.sync.get(["bgColor", "navColor", "textColor", "secondaryTextColor", "activeColor", "shadowColor"]);
     getting.then(setCurrentChoice);
-  }
+}
 
 //changing and saving settings:
 document.addEventListener("DOMContentLoaded", restoreOptions);
@@ -155,7 +159,7 @@ document.getElementById('btn-save').onclick = function savingSettings() {
     var thumb = document.getElementById("thumb-chk");
     var compact = document.getElementById("compact-chk");
     var list = document.getElementById("list-chk");
-    
+
     var dark = document.getElementById("dark-chk");
 
     browser.storage.sync.set({
@@ -163,8 +167,8 @@ document.getElementById('btn-save').onclick = function savingSettings() {
     })
 
     window.location.reload(false); //reloading page when settings are saved
-    
-    if(dark.checked == true) {
+
+    if (dark.checked == true) {
         browser.storage.sync.set({
             bgColor: "#3d3d3d",
             navColor: "#212121",
@@ -172,9 +176,8 @@ document.getElementById('btn-save').onclick = function savingSettings() {
             secondaryTextColor: "#bcbcbc",
             activeColor: "#f0f0f0",
             shadowColor: "#494949"
-          });
-    }
-    else {
+        });
+    } else {
         browser.storage.sync.set({
             bgColor: "#fcfcfc",
             navColor: "#fafafa",
@@ -182,16 +185,14 @@ document.getElementById('btn-save').onclick = function savingSettings() {
             secondaryTextColor: "#8a8a8a",
             activeColor: "#3f3f3f",
             shadowColor: "#e9e9e9"
-          });
+        });
     }
 
-    if(thumb.checked == true) {
+    if (thumb.checked == true) {
         console.log("thumb");
-    }
-    else if(compact.checked == true) {
+    } else if (compact.checked == true) {
         console.log("compact");
-    }
-    else if(list.checked == true) {
+    } else if (list.checked == true) {
         console.log("list");
     }
 }
@@ -200,10 +201,11 @@ document.getElementById('btn-save').onclick = function savingSettings() {
 document.getElementById('user-button-add').onclick = function selectBookmarknTag() {
     var box = document.getElementById("user-add-select");
 
-    if(box.style.display == "flex") {
+    if (box.style.display == "flex") {
         box.style.display = "none";
+    } else {
+        box.style.display = "flex";
     }
-    else{box.style.display = "flex";}
 
 }
 
@@ -211,39 +213,43 @@ document.getElementById('user-button-add').onclick = function selectBookmarknTag
 document.getElementById('select-tag').onclick = function newTag() {
     /* displaying menu to create new tag */
     var box = document.getElementById("new-tag-content");
-    if(box.style.display == "flex") {box.style.display = "none";}
-    else{box.style.display = "flex";}
+    if (box.style.display == "flex") {
+        box.style.display = "none";
+    } else {
+        box.style.display = "flex";
+    }
 
     /* creating and saving new tag */
     document.getElementById('new-tag-create').onclick = function tagMaker() {
 
         var userInput = document.getElementById("new-tag-input").value;
-        if(userInput.length != 0) {
-        function idNewSearch(bookmarkItems) {
-            for (item of bookmarkItems) {
-                //console.log(item.id);
+        if (userInput.length != 0) {
+            function idNewSearch(bookmarkItems) {
+                for (item of bookmarkItems) {
+                    //console.log(item.id);
 
-                function onCreated(node) {
-                    box.style.display = "none";
-                    window.location.reload(false);
-                    //browser.storage.sync.set({ })
+                    function onCreated(node) {
+                        box.style.display = "none";
+                        window.location.reload(false);
+                        //browser.storage.sync.set({ })
+                    }
+
+                    var createBookmark = browser.bookmarks.create({
+                        title: userInput,
+                        parentId: item.id
+                    });
+
+                    createBookmark.then(onCreated);
+                    document.getElementById("new-tag-input").value = "";
                 }
-
-                var createBookmark = browser.bookmarks.create({
-                    title: userInput,
-                    parentId: item.id
-                });
-
-                createBookmark.then(onCreated);
-                document.getElementById("new-tag-input").value = "";
             }
-        }
-        
-        var gettingID = browser.bookmarks.search({title : "speeddial"});
-        gettingID.then(idNewSearch);
 
-        }
-        else{
+            var gettingID = browser.bookmarks.search({
+                title: "speeddial"
+            });
+            gettingID.then(idNewSearch);
+
+        } else {
             document.getElementById("input-error").style.display = "block";
         }
     }
