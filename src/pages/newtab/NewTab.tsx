a3168baa0609b540fbe6ddfcba0cd57a9f3dd37d
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2022-04-30 17:25:43 +0200

    add heroicons, start creating new tab page

--- src/pages/newtab/NewTab.tsx
+++ src/pages/newtab/NewTab.tsx
@@ -1,10 +1,44 @@
-import { Button } from '@mantine/core';
+import { Button, Grid, Group, Text, Title } from '@mantine/core';
+import { CogIcon, PlusIcon } from '@heroicons/react/outline';
+
+import { NewTabLayout } from './NewTab.styles';
 
 export const NewTab = () => {
 	return (
-		<div>
-			<h2>new tab page here...</h2>
-			<Button>Hello?</Button>
-		</div>
+		<NewTabLayout>
+			<Grid columns={3} style={{ marginBottom: 32 }}>
+				<Grid.Col span={1}>
+					<Title>Simple Start</Title>
+				</Grid.Col>
+
+				<Grid.Col span={1} offset={1}>
+					<Group position="right" spacing="xs">
+						<Button
+							variant="subtle"
+							leftIcon={<PlusIcon style={{ width: 14, height: 14 }} />}
+							compact
+							color="dark"
+						>
+							Add
+						</Button>
+
+						<Button
+							variant="subtle"
+							leftIcon={<CogIcon style={{ width: 14, height: 14 }} />}
+							compact
+							color="dark"
+						>
+							Settings
+						</Button>
+					</Group>
+				</Grid.Col>
+			</Grid>
+
+			<div>
+				<Text color="dimmed">
+					Click "add" button to add your first bookmark to this view.
+				</Text>
+			</div>
+		</NewTabLayout>
 	);
 };
