fe74cde39e062dddc170bbf8a73dc8c711de0038
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2022-04-30 22:54:25 +0200

    add newBookmark modal, use Menu component for the "add new" button

---
+++ src/forms/NewBookmarkForm/index.ts
@@ -0,0 +1,1 @@
+export { NewBookmarkForm } from './NewBookmarkForm';
--- src/pages/newtab/NewTab.tsx
+++ src/pages/newtab/NewTab.tsx
@@ -1,44 +1,106 @@
-import { Button, Grid, Group, Text, Title } from '@mantine/core';
-import { CogIcon, PlusIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+import { Button, Grid, Group, Menu, Text, Title, Modal } from '@mantine/core';
+import { CogIcon, PlusIcon, BookmarkIcon, CollectionIcon } from '@heroicons/react/outline';
 
 import { NewTabLayout } from './NewTab.styles';
+import { NewBookmarkForm } from '../../forms/NewBookmarkForm';
 
 export const NewTab = () => {
+	const [newBookmarkModal, setNewBookmarkModal] = useState(false);
+
 	return (
-		<NewTabLayout>
-			<Grid columns={3} style={{ marginBottom: 32 }}>
-				<Grid.Col span={1}>
-					<Title>Simple Start</Title>
-				</Grid.Col>
+		<>
+			<NewTabLayout>
+				<Grid columns={3} style={{ marginBottom: 32 }}>
+					<Grid.Col span={1}>
+						<Title>Simple Start</Title>
+					</Grid.Col>
 
-				<Grid.Col span={1} offset={1}>
-					<Group position="right" spacing="xs">
-						<Button
-							variant="subtle"
-							leftIcon={<PlusIcon style={{ width: 14, height: 14 }} />}
-							compact
-							color="dark"
-						>
-							Add
-						</Button>
+					<Grid.Col span={1} offset={1}>
+						<Group position="right" spacing="xs">
+							<Menu
+								control={
+									<Button
+										variant="subtle"
+										leftIcon={<PlusIcon style={{ width: 14, height: 14 }} />}
+										compact
+										color="dark"
+										sx={(theme) => ({
+											'&:hover': {
+												backgroundColor: theme.colors.gray[2],
+											},
+										})}
+									>
+										Add
+									</Button>
+								}
+								withArrow
+							>
+								<Menu.Item
+									icon={<BookmarkIcon style={{ width: 14, height: 14 }} />}
+									sx={(theme) => ({
+										'&:hover': {
+											backgroundColor: theme.colors.gray[2],
+										},
+									})}
+									onClick={() => setNewBookmarkModal(true)}
+								>
+									New Bookmark
+								</Menu.Item>
+								<Menu.Item
+									icon={<CollectionIcon style={{ width: 14, height: 14 }} />}
+									sx={(theme) => ({
+										'&:hover': {
+											backgroundColor: theme.colors.gray[2],
+										},
+									})}
+								>
+									New Category
+								</Menu.Item>
+							</Menu>
 
-						<Button
-							variant="subtle"
-							leftIcon={<CogIcon style={{ width: 14, height: 14 }} />}
-							compact
-							color="dark"
-						>
-							Settings
-						</Button>
-					</Group>
-				</Grid.Col>
-			</Grid>
+							<Button
+								variant="subtle"
+								leftIcon={<CogIcon style={{ width: 14, height: 14 }} />}
+								compact
+								color="dark"
+								sx={(theme) => ({
+									'&:hover': {
+										backgroundColor: theme.colors.gray[2],
+									},
+								})}
+							>
+								Settings
+							</Button>
+						</Group>
+					</Grid.Col>
+				</Grid>
 
-			<div>
-				<Text color="dimmed">
-					Click "add" button to add your first bookmark to this view.
-				</Text>
-			</div>
-		</NewTabLayout>
+				<div>
+					<Text color="dimmed">
+						Click "add" button to add your first bookmark to this view.
+					</Text>
+				</div>
+			</NewTabLayout>
+
+			<Modal
+				opened={newBookmarkModal}
+				onClose={() => setNewBookmarkModal(false)}
+				centered
+				title="Add New Bookmark"
+				size="lg"
+				styles={(theme) => ({
+					title: {
+						fontSize: theme.headings.sizes.h3.fontSize,
+						fontWeight: 'bold',
+					},
+					close: {
+						color: theme.colors.red[6],
+					},
+				})}
+			>
+				<NewBookmarkForm />
+			</Modal>
+		</>
 	);
 };
