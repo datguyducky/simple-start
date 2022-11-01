a25a6de62158ae8f60caef669f527d527f1a512c
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2022-11-01 23:31:20 +0100

    validate remaining forms with zod

---
+++ src/validation/bookmarkSchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const bookmarkSchema = z.object({
+	bookmarkName: z.string().min(1, { message: 'Bookmark name is required' }),
+	bookmarkUrl: z
+		.string()
+		.min(1)
+		.regex(/^(?:f|ht)tps?:\/\//, { message: 'Bookmark URL needs to start with "https://"' }),
+	bookmarkCategoryId: z.string(),
+});
